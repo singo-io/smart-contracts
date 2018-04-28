657ffcd8c02e5908db371750aa249355ac0a1958
Peter Tulala <peter.tulala@adspired.com>
2018-04-28 16:33:00 +0200

    Ledger Web3 Provider

--- truffle.js
+++ truffle.js
@@ -1,11 +1,32 @@
+var infura_apikey = "QnfxPf19S2Zyd8b7HsDx";
+
+var LedgerWalletProvider = require("truffle-ledger-provider");
+
+var ledgerOptions = {
+  networkId: 3,
+  accountsOffset: 5
+};
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
   networks: {
     development: {
-    host: "localhost",
-    port: 7545,
-    network_id: "*" // Match any network id
-   }
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+      gas: 4000000,
+      gasPrice: 1e8
+    },
+    ropsten: {
+      provider: new LedgerWalletProvider(ledgerOptions, "https://ropsten.infura.io/" + infura_apikey),
+      network_id: 3,
+      gas: 4000000,
+      gasPrice: 1e8
+    }
   }
 };
